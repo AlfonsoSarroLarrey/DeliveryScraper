3004b6dac27f3183278e1db524e69e7aab1ce6d8
Alfonso <alfonsosarrolarrey@gmail.com>
2025-07-01 15:58:33 +0200

    Initial

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+
+URL_PAGE = "https://deliveroo.co.uk/"
+
+driver = webdriver.Chrome()
+driver.get()
