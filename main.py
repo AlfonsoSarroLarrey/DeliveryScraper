81d202b694a5c9d42e23961646df9b4f8d5ec100
Alfonso <alfonsosarrolarrey@gmail.com>
2025-07-15 09:44:51 +0200

    New restaurants selection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 
@@ -11,6 +9,9 @@ from bs4 import BeautifulSoup
 
 postcode = 'SW1A 2AA'
 URL_PAGE = f'https://deliveroo.co.uk/restaurants/london/westminster?postcode={postcode}&collection=all-restaurants'
+
+RESTAURANT_TAG_CLASS = 'HomeFeedGrid-b0432362335be7af'
+RATING_TAG_CLASS = 'css-87fssf'
 
 initial_load_time = 2
 scroll_pause_time = 0.0001
@@ -21,9 +22,10 @@ driver.get(URL_PAGE)
 
 time.sleep(initial_load_time)
 screen_height = driver.execute_script('return window.screen.height;')
+#Click on deny cookies
 driver.execute_script('document.querySelector("#onetrust-reject-all-handler").click()')
+#Click on accept coupon
 driver.execute_script('document.querySelector(".ccl-388f3fb1d79d6a36.ccl-6d2d597727bd7bab.ccl-59eced23a4d9e077.ccl-7be8185d0a980278").click()')
-#driver.execute_script('document.querySelector(".ccl-388f3fb1d79d6a36.ccl-9ed29b91bb2d9d02.ccl-59eced23a4d9e077.ccl-7be8185d0a980278").click()')
 
 i = 1
 while True:
@@ -38,8 +40,29 @@ while True:
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 driver.quit()
 
-a_tag_class = 'HomeFeedGrid-b0432362335be7af'
+data = []
+for restaurant in soup.find_all(class_ = RESTAURANT_TAG_CLASS):
+    rating = restaurant.find(class_ = RATING_TAG_CLASS)
 
-restaurants_html = [str(x) for x in soup.find_all(class_ = a_tag_class)]
+    if rating == None:
+        #Skip iteration
+        continue
+    if rating.text == 'New on Deliveroo':
+        print(restaurant.prettify())
+        #TODO: ENTRAR A PAGINA WEB ESPECIFICA DE RESTAURANTE (DELIVEROO) Y SACAR LA INFO POSTEADA
+        #TODO: INVESTIGAR COMO CREAR OBJETO JSON Y DUMPEAR INFO NECESARIA
+    
+    
+    
+with open('output.json', 'w') as file:
+    json.dump(data, file, indent=4)
 
-print(restaurants_html[0])
+
+#new_restaurants = []
+
+#for restaurant in restaurants_html:
+    #rating = restaurant.find('span', attrs={'class': 'ccl-649204f2a8e630fd ccl-6f43f9bb8ff2d712'}).text.strip()
+    #if rating == "New on Deliveroo":
+        #new_restaurants.append(rating)
+
+#print(new_restaurants)
